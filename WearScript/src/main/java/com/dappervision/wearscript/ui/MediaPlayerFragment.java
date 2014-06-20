wearscript/wearscript-android

Add method WS.Media.seekBackwards for seeking to a point in the video at a certain time from the end, rather than the beginning

WearScript/src/main/java/com/dappervision/wearscript/ui/MediaPlayerFragment.java
@@ -107,26 +107,28 @@ public class MediaPlayerFragment extends GestureFragment implements MediaPlayer.
             mp.pause();
         } else if (action.equals("playReverse"))
         {
-            playReverse(e.getMagnitude());
+            playReverse(e.getMsecs());
         }
         else if (action.equals("jump"))
         {
             interrupt=true;
-            jump(e.getMagnitude());
+            jump(e.getMsecs());
         }
         else if (action.equals("playFastForward"))
         {
-              playFastForward(e.getMagnitude());
+              playFastForward(e.getMsecs());
         }
         else if (action.equals("rewind"))
         {
-            rewind(e.getMagnitude());
+            rewind(e.getMsecs());
         }
         else if (action.equals("fastForward"))
         {
-            fastForward(e.getMagnitude());
+            fastForward(e.getMsecs());
         } else if (action.equals("seekTo")) {
-            mp.seekTo(e.getMagnitude());
+            mp.seekTo(e.getMsecs());
+        } else if (action.equals("seekBackwards")) {
+            mp.seekTo(mp.getDuration() - e.getMsecs());
         }
     }
 

scottgwald/wearscript

WearScript/src/main/java/com/dappervision/wearscript/ui/MediaPlayerFragment.java
@@ -98,18 +98,20 @@ public class MediaPlayerFragment extends GestureFragment implements MediaPlayer.
             interrupt = true;
             mp.pause();
         } else if (action.equals("playReverse")) {
-            playReverseFromEnd(e.getMagnitude());
+            playReverseFromEnd(e.getMsecs());
         } else if (action.equals("jump")) {
             interrupt = true;
-            jump(e.getMagnitude());
+            jump(e.getMsecs());
         } else if (action.equals("playFastForward")) {
-            playFastForwardFromBeginning(e.getMagnitude());
+            playFastForwardFromBeginning(e.getMsecs());
         } else if (action.equals("rewind")) {
-            rewind(e.getMagnitude());
+            rewind(e.getMsecs());
         } else if (action.equals("fastForward")) {
-            fastForward(e.getMagnitude());
+            fastForward(e.getMsecs());
         } else if (action.equals("seekTo")) {
-            mp.seekTo(e.getMagnitude());
+            mp.seekTo(e.getMsecs());
+        } else if (action.equals("seekBackwards")) {
+            mp.seekTo(mp.getDuration() - e.getMsecs());
         }
     }
 