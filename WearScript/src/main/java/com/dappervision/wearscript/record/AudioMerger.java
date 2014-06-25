bc0fd68496c4f3d0d16ad4e5775f1c1f9e7be7fd
Blake Elias <blakeelias@gmail.com>
2014-06-25 11:43:13 -0400

    Add method stub merge(File, File)

--- WearScript/src/main/java/com/dappervision/wearscript/record/AudioMerger.java
+++ WearScript/src/main/java/com/dappervision/wearscript/record/AudioMerger.java
@@ -1,4 +1,9 @@
 package com.dappervision.wearscript.record;
 
+import java.io.File;
+
 public class AudioMerger {
+    public static File merge(File file1, File file2) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 }
