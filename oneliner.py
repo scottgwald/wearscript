2c0c8505f444eef1d2bef11b3f6136582971baf1
Kurt Nelson <kurtisnelson@gmail.com>
2014-10-07 20:11:05 -0400

    Bump one-liner version number

--- oneliner.py
+++ oneliner.py
@@ -9,7 +9,7 @@ import subprocess
 import webbrowser
 import stat
 
-URL = 'https://github.com/kurtisnelson/wearscript-android/releases/download/v0.3.0/'
+URL = 'https://github.com/kurtisnelson/wearscript-android/releases/download/v0.4.0/'
 
 def browser(url):
     print('Open this url in a browser: ' + url)
